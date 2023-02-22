7582449c9cfdde12def086734855936089a3fc22
Chet Russell <chet_dylan@outlook.com>
2023-02-21 17:25:05 -0700

    Updated project

--- MLWIC2_helper_files/debug_run.py
+++ MLWIC2_helper_files/debug_run.py
@@ -1,5 +1,3 @@
 import run
 
-run(
-    "eval --val_info /home/chet/documents/mlwic_python3/mlwic_examples-master/image_labels.csv --path_prefix /home/chet/documents/mlwic_python3/mlwic_examples-master/images --log_dir /home/chet/documents/mlwic_python3/mlwic2_helper_files/empty_animal"
-)
+run("eval --val_info /home/chet/documents/mlwic_python3/mlwic_examples-master/image_labels.csv --path_prefix /home/chet/documents/mlwic_python3/mlwic_examples-master/images --log_dir /home/chet/documents/mlwic_python3/mlwic2_helper_files/empty_animal")
